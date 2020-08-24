cdcee558cc492a70ca8dcf75d15253474683d1ed
Thiago Vicente <thiago.vicente@tivit.com>
2020-08-24 01:15:22 -0300

    crud article vuejs

--- article/urls.py
+++ article/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 from core.router import router
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include(router.urls))
+    path("api/", include(router.urls)),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
