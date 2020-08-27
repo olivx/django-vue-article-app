ff2e8a5c02a45cc128382662dd88bda1a65c6ad6
Thiago Vicente <thiago.vicente@tivit.com>
2020-08-27 14:15:03 -0300

    Closes #5 add text editor to body

--- core/models.py
+++ core/models.py
@@ -5,6 +5,9 @@ class Article(models.Model):
     title =  models.CharField(max_length=250)
     body = models.TextField()
 
+    class Meta:
+        ordering = ["-id"]
+
     def __str__(self):
         return self.title
 
